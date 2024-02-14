6756b8f3ed5ec5de2467b9cbf9d94448afe73e89
DeAngelo Jones <deangelojones0708@gmail.com>
2024-02-14 17:15:00 -0600

    fixed bug with images

--- src/components/ProjectInfo.jsx
+++ src/components/ProjectInfo.jsx
@@ -4,7 +4,7 @@ export const projects = [
         id: 1,
         name: 'NoSQL Social-Network-API',
         description: 'An API for a social network web application where users can share their thoughts, react to friends` thoughts, and create a friend list',
-        image: 'https://private-user-images.githubusercontent.com/142169871/300299192-282f51b7-2904-4b4d-ae1e-f335c66a0757.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDc1MTU4MDEsIm5iZiI6MTcwNzUxNTUwMSwicGF0aCI6Ii8xNDIxNjk4NzEvMzAwMjk5MTkyLTI4MmY1MWI3LTI5MDQtNGI0ZC1hZTFlLWYzMzVjNjZhMDc1Ny5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMjA5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDIwOVQyMTUxNDFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT01NjY2ZWUwYmRiYTZhMDY0NDk2YTliNGZhYjIzZmI4ZTM1NjFjY2QxNGE3ODQzYzk2NmM2MWE3Y2E2Nzk2MmI3JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.dtO0Fxnu4GM3zhvxGe1wowDW8a4_IarbZT8kLxkpUp8',
+        image: 'https://github.com/dtjones23/ReactFolio/blob/main/src/images/nosql.social.png?raw=true',
         live: '',
         repo: 'https://github.com/dtjones23/NoSQL-SocialNetworkAPI'
     },
@@ -12,7 +12,7 @@ export const projects = [
         id: 2,
         name: 'Primary Games',
         description: 'A digital game-link center that grants users the ability to browse across video games. Users can search for games by name, genre, or platform. Users can also create an account and save games to their profile.',
-        image: 'https://private-user-images.githubusercontent.com/142169871/292356934-b088586a-5825-40d0-9144-286fd79aad26.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDc1MTU4OTUsIm5iZiI6MTcwNzUxNTU5NSwicGF0aCI6Ii8xNDIxNjk4NzEvMjkyMzU2OTM0LWIwODg1ODZhLTU4MjUtNDBkMC05MTQ0LTI4NmZkNzlhYWQyNi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMjA5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDIwOVQyMTUzMTVaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT03NjdhMjA2MDBiZTI2MTViZjQ2YmNiZjIyNTcxMTkwN2JiM2EzNDk4YWVjYWZmMWVkMTM5YjFhYmZlODY2N2ZhJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.5dERmOtIGyN1mJ4A6AzkWvBW1jSrBYOqVeK7VxtMM8g',
+        image: 'https://github.com/dtjones23/ReactFolio/blob/main/src/images/primarygames.png?raw=true',
         live: 'https://primary-games-app3-6a2841bf5a98.herokuapp.com/',
         repo: 'https://github.com/dtjones23/Primary-Games?tab=readme-ov-file'
     },
@@ -20,7 +20,7 @@ export const projects = [
         id: 3,
         name: 'Code Quiz Game',
         description: 'A timed quiz on JavaScript fundamentals that stores high scores',
-        image: 'https://private-user-images.githubusercontent.com/142169871/302135668-ef2e9b0b-b835-47b3-8c31-19d32c2bfa7c.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDc1MTU3MTYsIm5iZiI6MTcwNzUxNTQxNiwicGF0aCI6Ii8xNDIxNjk4NzEvMzAyMTM1NjY4LWVmMmU5YjBiLWI4MzUtNDdiMy04YzMxLTE5ZDMyYzJiZmE3Yy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMjA5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDIwOVQyMTUwMTZaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT00MjQ2YjgwOWRhYjIwMjg1MGIwYmQ5NmE3YmJmYmVkNTNkYTE1ZWE0ZmVjNGY1NmYwZmIyZjhiNjMxZGJmMTI0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.a-6sJU5lD5VEUtSxOZKtvNe5Q11WNHusEOzlYH6wzio',
+        image: 'https://github.com/dtjones23/ReactFolio/blob/main/src/images/code.quiz.png?raw=true',
         live: 'https://dtjones23.github.io/Code-Quiz-Repo/',
         repo: 'https://github.com/dtjones23/Code-Quiz-Repo?tab=readme-ov-file'
     },
@@ -44,7 +44,7 @@ export const projects = [
         id: 6,
         name: 'Work Day Scheduler',
         description: 'A simple calendar application that allows the user to save events for each hour of the day',
-        image: '/Users/deangelojones/Homework/ReactFolio/src/images/sqlbusinesstrack.jpeg',
+        image: 'https://github.com/dtjones23/ReactFolio/blob/main/src/images/dayplanner.png?raw=true',
         live: 'https://dtjones23.github.io/Day-Planning-Calender/',
         repo: 'https://github.com/dtjones23/Day-Planning-Calender'
     }
